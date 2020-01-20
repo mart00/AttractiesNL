e6dcb24f71ac56947fb1aad5ee86f241da0e00ba
mart00 <47355471+mart00@users.noreply.github.com>
2020-01-20 15:32:17 +0100

    Laatste

--- app/src/main/java/com/example/attractienl/MainActivity.java
+++ app/src/main/java/com/example/attractienl/MainActivity.java
@@ -4,28 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
+    listAdapter lis;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
+
         final Context context = this;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         viewPagerAdapter view = new viewPagerAdapter(context);
-       final String[] itemname = view.getNamen();
+
+        final String[] itemname = view.getNamen();
         String[] plaats = view.getPlaats();
         Integer[] images = view.getImages();
 
-        listAdapter lis = new listAdapter(this,itemname,plaats,images);
-
+        lis = new listAdapter(this,itemname,plaats,images);
         ListView list;
 
-
-
         list = findViewById(R.id.list);
+        EditText editText = findViewById(R.id.editText);
         list.setAdapter(lis);
 
         list.setOnItemClickListener(new AdapterView.OnItemClickListener(){
@@ -36,6 +45,22 @@ public class MainActivity extends AppCompatActivity {
                 Integer itemClicked = position+1;
                 intent.putExtra("itemname", itemClicked);
                 startActivity(intent);
+            }
+        });
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                (MainActivity.this).lis.getFilter().filter(s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
             }
         });
     }
